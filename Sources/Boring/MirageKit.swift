d00e9faab1a1ac736978ed596e64f9017549dbb4
Michael Wells <michael@michaelwells.com>
2026-03-18 19:17:29 -0700

    0.5.0

---
+++ Sources/Boring/MirageKit.swift
@@ -0,0 +1,8 @@
+//
+// Copyright 2025-2026 Wells Consulting.
+// This file is part of MirageKit and is released under the MIT License.
+//
+
+public enum MirageKit {
+    public static let version = "0.5.0"
+}
